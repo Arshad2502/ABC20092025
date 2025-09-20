84422a831fa943da401e03c523972823f1235217
Arshad2502 <arshad.jhs25@gmail.com>
2025-09-20 09:00:14 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "resource_group_name"{
+    default="tfrg"
+}
+variable "resource_group_location"{
+    default="Australia East"
+}
+variable "virtual_network_name"{
+    default="agw"
+}
+variable "subnet_name"{
+    default="tfvirtualsubnet"
+}
+variable "pip_name"{
+    default="tfpip"
+}
+variable "agw_network"{
+    default="taskagw"
+}
